6e1f972444e40f4f668de8f9b726be001e7092c4
Abhishek Kumar Yadav <abhishekyadv347754@gmail.com>
2024-06-22 21:53:07 +0530

    linkedlist by collection

---
+++ LinkedList/LLbyCollection.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class LLbyCollection {
+    public static void main(String args[]){
+        LinkedList<Integer> l1=new LinkedList<>();
+        l1.addFirst(40);
+        l1.addFirst(30);
+        l1.add(50);
+        l1.addLast(60);
+        System.out.println(l1);
+     }
+}
