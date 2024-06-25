ace07b65847c2f00e7905ce33a60dac3cab0941a
Abhishek Kumar Yadav <abhishekyadv347754@gmail.com>
2024-06-26 00:47:52 +0530

    stack using linked list and collections

---
+++ Stacks/StackColletion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class StackColletion {
+    public static void main(String args[]){
+        Stack<Integer> s=new Stack<>();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s);
+    }
+
+}
