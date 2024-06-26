c924285e96df9c86940dd3a1cc32ae5084f70c44
Abhishek Kumar Yadav <abhishekyadv347754@gmail.com>
2024-06-26 14:12:57 +0530

    stack using linked list and collections

--- Stacks/StackColletion.java
+++ Stacks/StackColletion.java
@@ -9,13 +9,37 @@ public class StackColletion {
         pushAtBottom(s,data);
         s.push(top);
     }
+    public static String reverseString(String str){
+        Stack<Character> s=new Stack<>();
+        int idx=0;
+        while (idx < str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder result=new StringBuilder("");
+        while(!s.isEmpty()){
+            char curr=s.pop();
+            result.append(curr);
+        }
+        System.out.println(result);
+        String str1=result.toString();
+        System.out.println(str1.toCharArray());
+        return str1;
+
+    }
     public static void main(String args[]){
-        Stack<Integer> s=new Stack<>();
-        s.push(10);
-        s.push(20);
-        s.push(30);
-        pushAtBottom(s,4);
-        System.out.println(s);
+       // Stack<Integer> s=new Stack<>();
+       // s.push(10);
+       // s.push(20);
+       // s.push(30);
+       // pushAtBottom(s,4);
+       // System.out.println(s);
+        String str="abc";
+        System.out.print(reverseString(str));
+
+
+
+
     }
 
 }
