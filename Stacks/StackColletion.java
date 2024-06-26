97aa4b8db9240fbbe01d3dcf421a593eb8f9960b
Abhishek Kumar Yadav <abhishekyadv347754@gmail.com>
2024-06-26 12:01:36 +0530

    stack using linked list and collections

--- Stacks/StackColletion.java
+++ Stacks/StackColletion.java
@@ -1,10 +1,20 @@
 import java.util.*;
 public class StackColletion {
+    public static void pushAtBottom(Stack<Integer> s,int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top=s.pop();
+        pushAtBottom(s,data);
+        s.push(top);
+    }
     public static void main(String args[]){
         Stack<Integer> s=new Stack<>();
         s.push(10);
         s.push(20);
         s.push(30);
+        pushAtBottom(s,4);
         System.out.println(s);
     }
 
