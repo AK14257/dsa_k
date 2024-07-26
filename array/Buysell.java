4c5b20d525fff54b0a88d9fb119fb29e3c4bcb03
Abhishek Kumar Yadav <abhishekyadv347754@gmail.com>
2024-07-26 17:35:39 +0530

    buy sell

---
+++ array/Buysell.java
@@ -0,0 +1,20 @@
+public class Buysell {
+    public static int BuySell(int prices[]){
+        int buyPrice=Integer.MAX_VALUE;
+        int maxProfit=0;
+        for(int i=0;i<prices.length;i++){
+            if(buyPrice<prices[i]){
+                int profit=prices[i]-buyPrice;
+                maxProfit=Math.max(maxProfit,profit);
+            }
+            else{
+                buyPrice=prices[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String args[]) {
+        int arr[] = {7, 1, 5, 3, 6, 4};
+        System.out.print(BuySell(arr));
+
+    }}
